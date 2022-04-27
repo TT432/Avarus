437310a7d4af3bf2c321e85f45745f5c105a6e94
TT432 <2437315224@qq.com>
2022-04-27 23:23:35 +0800

    美术资源

--- src/main/java/com/nmmoc7/avarus/datagen/TutItemModels.java
+++ src/main/java/com/nmmoc7/avarus/datagen/TutItemModels.java
@@ -1,11 +1,12 @@
 package com.nmmoc7.avarus.datagen;
 
 import com.nmmoc7.avarus.Avarus;
-import com.nmmoc7.avarus.item.AvarusItems;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -23,22 +24,27 @@ public class TutItemModels extends ItemModelProvider {
     @Override
     protected void registerModels() {
         ForgeRegistries.ITEMS.forEach(item -> {
-            if (item.getRegistryName().getNamespace().equals(Avarus.MOD_ID) && item instanceof BlockItem) {
-                withExistingParent(item.getRegistryName().getPath(), modLoc("block/" + item.getRegistryName().getPath()));
+            if (item.getRegistryName().getNamespace().equals(Avarus.MOD_ID)) {
+                if (item instanceof BlockItem) {
+                    withExistingParent(item.getRegistryName().getPath(), modLoc("block/" + item.getRegistryName().getPath()));
+                }
+                else {
+                    simpleTexture(() -> item);
+                }
             }
         });
-
-        simpleTexture(AvarusItems.COLORFUL_ALLOY);
-        simpleTexture(AvarusItems.COLORFUL_ALLOY_INGOT);
-        simpleTexture(AvarusItems.COLORFUL_POWDER);
-        simpleTexture(AvarusItems.IRON_PLATE);
     }
 
     void simpleTexture(Supplier<Item> itemSupplier) {
         String name = itemSupplier.get().getRegistryName().getPath();
 
-        singleTexture(name,
-                mcLoc("item/generated"),
-                "layer0", modLoc("item/" + name));
+        try {
+            ResourceLocation texture = modLoc("item/" + name);
+
+            if (existingFileHelper.exists(texture, ModelProvider.TEXTURE)) {
+                singleTexture(name, mcLoc("item/generated"), "layer0", texture);
+            }
+        }
+        catch (Exception ignored) {}
     }
 }
